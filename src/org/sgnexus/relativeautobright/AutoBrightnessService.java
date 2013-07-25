537966c92410d08ac76258fbcb4889137ceb8420
Korey Conway <korey.conway@gmail.com>
2013-07-24 21:31:30 -0400

    - connected relative slider to service through a binder - added some logging - added relative level into brightness calculation

--- src/org/sgnexus/relativeautobright/AutoBrightnessService.java
+++ src/org/sgnexus/relativeautobright/AutoBrightnessService.java
@@ -7,65 +7,138 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Binder;
 import android.os.IBinder;
 import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
 import android.util.Log;
-import android.widget.Toast;
 
 public class AutoBrightnessService extends Service implements
 		SensorEventListener {
-	private final int SENSOR_DELAY = 3000;
 	private final int MAX_BRIGHTNESS = 255;
 	private final int MIN_BRIGHTNESS = 0;
 	private String mTag = this.getClass().getSimpleName();
 	private SensorManager mSensorManager;
-	private int relativeLevel = 10;
+	private int mRelativeLevel = -1;
+	private int mCurrentBrightness;
+	private float mCurrentLux;
+	private int mTargetBrightness;
+	private boolean mRunning = false;
+
+	@Override
+	public void onCreate() {
+		Log.d(mTag, "creating service");
+		super.onCreate();
+
+		// Get the current system brightness
+		try {
+			mCurrentBrightness = Settings.System.getInt(getContentResolver(),
+					Settings.System.SCREEN_BRIGHTNESS);
+		} catch (SettingNotFoundException e) {
+			mCurrentBrightness = 0;
+		}
+
+		// Setup the light sensor
+		mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+	}
+	
+	private void init(int initialRelativeLevel) {
+		if (!mRunning) {
+			mRunning = true;
+			mRelativeLevel = initialRelativeLevel;
+			Sensor lightSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+			mSensorManager.registerListener(this, lightSensor,
+					SensorManager.SENSOR_DELAY_NORMAL);
+		}
+	}
 
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
-		// Settings.System.putInt(getContentResolver(),
-		// Settings.System.SCREEN_BRIGHTNESS_MODE,
-		// Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL);
-		mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-		Sensor lightSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
-		mSensorManager.registerListener(this, lightSensor, SENSOR_DELAY);
+		Log.d(mTag, "starting service");
+		init(intent.getExtras().getInt("relativeLevel"));
 		return super.onStartCommand(intent, flags, startId);
 	}
 
 	@Override
 	public IBinder onBind(Intent intent) {
-		return null;
+		Log.d(mTag, "binding service");
+		init(intent.getExtras().getInt("relativeLevel"));
+		return new BrightnessBinder();
 	}
 
-	public void setBrightness(int brightness) {
-		brightness = Math.max(Math.min(brightness, MAX_BRIGHTNESS),
+	public void setRelativeLevel(int level) {
+		mRelativeLevel = level;
+		Log.d(mTag, "new level: " + level);
+		computeTargetBrightness();
+	}
+
+	private void setTargetBrightness(int brightness) {
+		// Stop the service since user is using system auto brightness
+		if (isUsingSystemAutoBrightness()) {
+			stopSelf();
+		}
+
+		mTargetBrightness = Math.max(Math.min(brightness, MAX_BRIGHTNESS),
 				MIN_BRIGHTNESS);
+		Log.d(mTag, "new target: " + mTargetBrightness);
+		updateScreenBrightness();
+	}
+
+	private void updateScreenBrightness() {
+		mCurrentBrightness = mTargetBrightness;
+		Log.d(mTag, "new current: " + mCurrentBrightness);
 		Settings.System.putInt(getContentResolver(),
-				Settings.System.SCREEN_BRIGHTNESS, brightness);
+				Settings.System.SCREEN_BRIGHTNESS, mCurrentBrightness);
+	}
+
+	private boolean isUsingSystemAutoBrightness() {
+		try {
+			int mode = Settings.System.getInt(getContentResolver(),
+					Settings.System.SCREEN_BRIGHTNESS_MODE);
+
+			if (mode == Settings.System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC) {
+				return true;
+			}
+
+			return false;
+		} catch (SettingNotFoundException e) {
+			return false;
+		}
 	}
 
 	@Override
 	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+		// Don't care about accuracy
 		return;
 	}
 
 	@Override
 	public void onSensorChanged(SensorEvent event) {
+		// Verify light sensor
 		if (event.sensor.getType() == Sensor.TYPE_LIGHT) {
-			float lux = event.values[0];
-			int newBrightness = (int) Math.log(lux) * relativeLevel;
-			setBrightness(newBrightness);
-
-			Log.d(mTag, "lux level: " + lux);
-			Log.d(mTag, "brightness level: " + newBrightness);
-			Toast.makeText(getApplicationContext(), "lux: " + lux,
-					Toast.LENGTH_SHORT).show();
+			// Get lux level
+			mCurrentLux = event.values[0];
+			computeTargetBrightness();
+			Log.d(mTag, "lux level: " + mCurrentLux);
 		}
+	}
+
+	private void computeTargetBrightness() {
+		// Compute a new target brightness level
+		int newBrightness = (int) Math.log(mCurrentLux) * 5 + mRelativeLevel;
+		setTargetBrightness(newBrightness);
 	}
 
 	@Override
 	public void onDestroy() {
+		Log.d(mTag, "killing service");
 		mSensorManager.unregisterListener(this);
 		super.onDestroy();
+	}
+
+	class BrightnessBinder extends Binder {
+		public AutoBrightnessService getService() {
+			return AutoBrightnessService.this;
+		}
 	}
 }
--- src/org/sgnexus/relativeautobright/MainActivity.java
+++ src/org/sgnexus/relativeautobright/MainActivity.java
@@ -1,26 +1,57 @@
 package org.sgnexus.relativeautobright;
 
+import org.sgnexus.relativeautobright.AutoBrightnessService.BrightnessBinder;
+
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.app.ActivityManager.RunningServiceInfo;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
-	private String mTag = MainActivity.class.getSimpleName();
+	// private String mTag = MainActivity.class.getSimpleName();
 	private CheckBox mVServiceEnabled;
+	private SeekBar mVBrightnessSeekBar;
+	private AutoBrightnessService mService;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		mVServiceEnabled = (CheckBox) findViewById(R.id.serviceEnabled);
+		mVBrightnessSeekBar = (SeekBar) findViewById(R.id.brightnessSeekBar);
+		
+		this.mVBrightnessSeekBar
+				.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+					@Override
+					public void onProgressChanged(SeekBar seekBar,
+							int progress, boolean fromUser) {
+						if (mService != null) {
+							mService.setRelativeLevel(progress);
+						}
+					}
+
+					@Override
+					public void onStartTrackingTouch(SeekBar seekBar) {
+						return;
+					}
+
+					@Override
+					public void onStopTrackingTouch(SeekBar seekBar) {
+						return;
+					}
+				});
 	}
 
 	@Override
@@ -39,13 +70,9 @@ public class MainActivity extends Activity {
 	public void toggleService(View v) {
 		Log.d("thread", "Activity Thread id: " + Thread.currentThread().getId());
 		if (isServiceRunning()) {
-			Toast.makeText(getApplicationContext(), "Stopping service",
-					Toast.LENGTH_SHORT).show();
-			stopService(new Intent(this, AutoBrightnessService.class));
+			stopService();
 		} else {
-			Toast.makeText(getApplicationContext(), "Starting service",
-					Toast.LENGTH_SHORT).show();
-			startService(new Intent(this, AutoBrightnessService.class));
+			startService();
 		}
 	}
 
@@ -60,4 +87,33 @@ public class MainActivity extends Activity {
 		}
 		return false;
 	}
+	
+	private void stopService() {
+		Toast.makeText(getApplicationContext(), "Stopping service",
+				Toast.LENGTH_SHORT).show();
+		this.unbindService(mConnection);
+		super.stopService(new Intent(this, AutoBrightnessService.class));
+	}
+	
+	private void startService() {
+		Toast.makeText(getApplicationContext(), "Starting service",
+				Toast.LENGTH_SHORT).show();
+		Intent intent = new Intent(this, AutoBrightnessService.class);
+		intent.putExtra("relativeLevel", mVBrightnessSeekBar.getProgress());
+		super.startService(intent);
+		bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+	}
+	
+	private ServiceConnection mConnection = new ServiceConnection() {
+		@Override
+		public void onServiceConnected(ComponentName className, IBinder binder) {
+			mService = ((BrightnessBinder)binder).getService();
+		}
+
+		@Override
+		public void onServiceDisconnected(ComponentName className) {
+			mService = null;
+		}
+	};
+
 }
