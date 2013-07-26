4334d7834fd87b5c4700c3c4c9e982173e76590a
Korey Conway <korey.conway@gmail.com>
2013-07-26 11:41:49 -0400

    - added useful constants - improved auto-brightness algorithm - when relative is min/max, brightness in min/max - toast notification / buzz when min/max is set in seek bar

--- src/org/sgnexus/relativeautobright/AutoBrightnessService.java
+++ src/org/sgnexus/relativeautobright/AutoBrightnessService.java
@@ -9,14 +9,19 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Binder;
 import android.os.IBinder;
+import android.os.Vibrator;
 import android.provider.Settings;
 import android.provider.Settings.SettingNotFoundException;
 import android.util.Log;
+import android.widget.Toast;
 
 public class AutoBrightnessService extends Service implements
 		SensorEventListener {
 	private final int MAX_BRIGHTNESS = 255;
 	private final int MIN_BRIGHTNESS = 0;
+	private final int MAX_LUX = 1000;
+	private final int MAX_RELATIVE = 100;
+	private final int MIN_RELATIVE = 0;
 	private String mTag = this.getClass().getSimpleName();
 	private SensorManager mSensorManager;
 	private int mRelativeLevel = -1;
@@ -41,14 +46,19 @@ public class AutoBrightnessService extends Service implements
 		// Setup the light sensor
 		mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 	}
-	
+
 	private void init(int initialRelativeLevel) {
 		if (!mRunning) {
 			mRunning = true;
 			mRelativeLevel = initialRelativeLevel;
-			Sensor lightSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+			Sensor lightSensor = mSensorManager
+					.getDefaultSensor(Sensor.TYPE_LIGHT);
 			mSensorManager.registerListener(this, lightSensor,
 					SensorManager.SENSOR_DELAY_NORMAL);
+			Settings.System.putInt(getContentResolver(),
+					Settings.System.SCREEN_BRIGHTNESS_MODE,
+					Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL);
+
 		}
 	}
 
@@ -59,29 +69,50 @@ public class AutoBrightnessService extends Service implements
 		return super.onStartCommand(intent, flags, startId);
 	}
 
-	@Override
-	public IBinder onBind(Intent intent) {
-		Log.d(mTag, "binding service");
-		init(intent.getExtras().getInt("relativeLevel"));
-		return new BrightnessBinder();
-	}
-
 	public void setRelativeLevel(int level) {
 		mRelativeLevel = level;
 		Log.d(mTag, "new level: " + level);
-		computeTargetBrightness();
+		updateTargetBrightness();
+		if (mRelativeLevel == MIN_RELATIVE) {
+			Toast.makeText(getApplicationContext(), "Min Brightness Set",
+					Toast.LENGTH_SHORT).show();
+
+			// Vibrate
+			((Vibrator) getSystemService(Context.VIBRATOR_SERVICE)).vibrate(150);
+		} else if (mRelativeLevel == MAX_RELATIVE) {
+			Toast.makeText(getApplicationContext(), "Max Brightness Set",
+					Toast.LENGTH_SHORT).show();
+
+			// Vibrate
+			((Vibrator) getSystemService(Context.VIBRATOR_SERVICE)).vibrate(150);
+		}
 	}
 
-	private void setTargetBrightness(int brightness) {
+	private void setTargetBrightness(int targetBrightness) {
+		mTargetBrightness = Math.min(MAX_BRIGHTNESS,
+				Math.max(MIN_BRIGHTNESS, targetBrightness));
+		Log.d(mTag, "new target: " + mTargetBrightness);
+		updateScreenBrightness();
+	}
+
+	private void updateTargetBrightness() {
 		// Stop the service since user is using system auto brightness
 		if (isUsingSystemAutoBrightness()) {
 			stopSelf();
 		}
 
-		mTargetBrightness = Math.max(Math.min(brightness, MAX_BRIGHTNESS),
-				MIN_BRIGHTNESS);
-		Log.d(mTag, "new target: " + mTargetBrightness);
-		updateScreenBrightness();
+		// Compute a new target brightness level
+		int newTarget;
+		if (mRelativeLevel == MIN_RELATIVE) {
+			newTarget = MIN_BRIGHTNESS;
+		} else if (mRelativeLevel == MAX_RELATIVE) {
+			newTarget = MAX_BRIGHTNESS;
+		} else {
+			newTarget = (int) ((mCurrentLux * MAX_BRIGHTNESS / MAX_LUX) + (mRelativeLevel
+					* MAX_BRIGHTNESS / MAX_RELATIVE / 3));
+		}
+
+		setTargetBrightness(newTarget);
 	}
 
 	private void updateScreenBrightness() {
@@ -118,15 +149,16 @@ public class AutoBrightnessService extends Service implements
 		if (event.sensor.getType() == Sensor.TYPE_LIGHT) {
 			// Get lux level
 			mCurrentLux = event.values[0];
-			computeTargetBrightness();
+			updateTargetBrightness();
 			Log.d(mTag, "lux level: " + mCurrentLux);
 		}
 	}
 
-	private void computeTargetBrightness() {
-		// Compute a new target brightness level
-		int newBrightness = (int) Math.log(mCurrentLux) * 5 + mRelativeLevel;
-		setTargetBrightness(newBrightness);
+	@Override
+	public IBinder onBind(Intent intent) {
+		Log.d(mTag, "binding service");
+		init(intent.getExtras().getInt("relativeLevel"));
+		return new BrightnessBinder();
 	}
 
 	@Override
