fbc5709b97d7a3c9a1735fc13432083907bfa109
Korey Conway <korey.conway@gmail.com>
2013-08-09 00:19:11 -0400

    Note: not a fully working commit - trying out single thread model - changed UI to use preferences (this is making some parts easier but others harder so far)

--- src/org/sgnexus/relativeautobright/MainActivity.java
+++ src/org/sgnexus/relativeautobright/MainActivity.java
@@ -1,112 +1,72 @@
 package org.sgnexus.relativeautobright;
 
-import org.sgnexus.relativeautobright.MainService.MainServiceBinder;
-
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.app.ActivityManager.RunningServiceInfo;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.ServiceConnection;
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
-import android.os.IBinder;
 import android.preference.PreferenceManager;
-import android.util.Log;
-import android.view.Menu;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.CompoundButton.OnCheckedChangeListener;
-import android.widget.SeekBar;
-import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.preference.SwitchPreference;
+import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
 import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements
+		OnSharedPreferenceChangeListener {
 	private String mTag = MainActivity.class.getSimpleName();
-	private CheckBox mVServiceEnabled;
-	private SeekBar mVBrightnessSeekBar;
-	private MainServiceBinder mServiceBinder;
+	private SettingsFragment mSettingsFragment;
 	private Toast mToast;
-	private SharedPreferences mPrefs;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// Load the UI
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
 
-		// Load the preferences
-		mPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+		// Set default settings (first run only)
+		PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
 
-		// Get and setup UI components
-		mVServiceEnabled = (CheckBox) findViewById(R.id.serviceEnabled);
-		mVServiceEnabled
-				.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-					@Override
-					public void onCheckedChanged(CompoundButton buttonView,
-							boolean isChecked) {
-						if (isChecked) {
-							startService();
-						} else {
-							stopService();
-						}
-
-					}
-				});
-		mVBrightnessSeekBar = (SeekBar) findViewById(R.id.brightnessSeekBar);
-		mVBrightnessSeekBar.setProgress(mPrefs.getInt("relativeLevel", 50));
-		mVBrightnessSeekBar
-				.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
-					@Override
-					public void onProgressChanged(SeekBar seekBar,
-							int progress, boolean fromUser) {
-						setRelativeLevel(progress);
-					}
-
-					@Override
-					public void onStartTrackingTouch(SeekBar seekBar) {
-						return;
-					}
-
-					@Override
-					public void onStopTrackingTouch(SeekBar seekBar) {
-						return;
-					}
-				});
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+		// Load the settings UI
+		mSettingsFragment = new SettingsFragment();
+		getFragmentManager().beginTransaction()
+				.replace(android.R.id.content, mSettingsFragment).commit();
 	}
 
 	@Override
 	protected void onResume() {
 		super.onResume();
-
-		// Check if we are even still running and update the checkbox
-		mVServiceEnabled.setChecked(isServiceRunning());
-
-		// Read the prefs then set seekbar progress (might have changed from
-		// notification area)
-		int relativeLevel = PreferenceManager.getDefaultSharedPreferences(this)
-				.getInt("relativeLevel", mVBrightnessSeekBar.getProgress());
-		mVBrightnessSeekBar.setProgress(relativeLevel);
-		
-		Log.d(mTag, "resuming");
+		PreferenceManager.getDefaultSharedPreferences(this)
+				.registerOnSharedPreferenceChangeListener(this);
+		((SwitchPreference) mSettingsFragment.findPreference("serviceEnabled"))
+				.setChecked(isServiceRunning());
 	}
 
-	private void setRelativeLevel(int relativeLevel) {
-		// Update service
-		if (mServiceBinder != null) {
-			mServiceBinder.setRelativeLevel(relativeLevel);
+	@Override
+	protected void onPause() {
+		PreferenceManager.getDefaultSharedPreferences(this)
+				.unregisterOnSharedPreferenceChangeListener(this);
+		super.onPause();
+	}
+
+	private void setServiceEnabled(boolean enabled) {
+		if (enabled) {
+			showToast("Starting service");
+			startService(new Intent(this, MainService.class));
+		} else {
+			showToast("Stopping service");
+			stopService(new Intent(this, MainService.class));
+		}
+	}
+
+	private void showToast(CharSequence msg) {
+		if (mToast != null) {
+			mToast.cancel();
 		}
 
-		// Save into preference file
-		mPrefs.edit().putInt("relativeLevel", relativeLevel).apply();
+		mToast = Toast.makeText(getApplicationContext(), msg,
+				Toast.LENGTH_SHORT);
+		mToast.show();
 	}
 
 	private boolean isServiceRunning() {
@@ -121,52 +81,14 @@ public class MainActivity extends Activity {
 		return false;
 	}
 
-	private void startService() {
-		toast("Starting service");
-		Intent intent = new Intent(this, MainService.class);
-		startService(intent);
-		bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
-	}
-
-	private void stopService() {
-		toast("Stopping service");
-		if (mServiceBinder != null) {
-			unbindService(mConnection);
-			mServiceBinder = null;
-		}
-		stopService(new Intent(this, MainService.class));
-	}
-
-	private void toast(CharSequence msg) {
-		if (mToast != null) {
-			mToast.cancel();
-		}
-
-		mToast = Toast.makeText(getApplicationContext(), msg,
-				Toast.LENGTH_SHORT);
-		mToast.show();
-	}
-
 	@Override
-	protected void onDestroy() {
-		if (mServiceBinder != null) {
-			unbindService(mConnection);
-			mServiceBinder = null;
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+			String key) {
+		if ("serviceEnabled".equals(key)) {
+			boolean enabled = sharedPreferences.getBoolean(key, false);
+			setServiceEnabled(enabled);
+		} else if ("relativeLevel".equals(key)) {
+			// TODO update the preference
 		}
-		super.onDestroy();
 	}
-
-	private ServiceConnection mConnection = new ServiceConnection() {
-		@Override
-		public void onServiceConnected(ComponentName className, IBinder binder) {
-			mServiceBinder = (MainServiceBinder) binder;
-		}
-
-		@Override
-		public void onServiceDisconnected(ComponentName className) {
-			Log.d(mTag, "unbinding this service");
-			mServiceBinder = null;
-		}
-	};
-
 }
--- src/org/sgnexus/relativeautobright/MainService.java
+++ src/org/sgnexus/relativeautobright/MainService.java
@@ -6,23 +6,50 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.os.Binder;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Looper;
 import android.preference.PreferenceManager;
+import android.provider.Settings;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.widget.RemoteViews;
 
-public class MainService extends Service {
+public class MainService extends Service implements
+		OnSharedPreferenceChangeListener, SensorEventListener {
 	private String mTag = this.getClass().getSimpleName();
-	private BackgroundThread mBgThread;
 	private ScreenReceiver mScreenReceiver;
+	private int mRelativeLevel;
+	private SharedPreferences mPrefs;
+	private SensorManager mSensorManager;
+	private Sensor mLightSensor;
+	private long mLastSenseMs = 0;
+	private long mSenseIntervalMs = 500;
+	private float mLux = 0;
+	private boolean mIsScreenOn = true;
 
-	void init() {
-		if (mBgThread == null) {
-			Log.d(mTag, "Creating service object");
-			int relativeLevel = PreferenceManager.getDefaultSharedPreferences(
-					this).getInt("relativeLevel", 50);
+	private static int LUX_DIFF_THRESHOLD = 0;
+
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		String action = intent.getAction();
+		if (action != null && action.equals("increase")) {
+			increaseBrightness();
+		} else if (action != null && action.equals("decrease")) {
+			decreaseBrightness();
+		} else {
+			Log.d(mTag, "starting service");
+
+			// Load settings
+			mPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+			mRelativeLevel = mPrefs.getInt("relativeLevel", 50);
+			mPrefs.registerOnSharedPreferenceChangeListener(this);
 
 			// Setup screen on/off detector
 			mScreenReceiver = new ScreenReceiver();
@@ -31,42 +58,112 @@ public class MainService extends Service {
 			filter.addAction(Intent.ACTION_SCREEN_OFF);
 			registerReceiver(mScreenReceiver, filter);
 
-			mBgThread = new BackgroundThread(this, relativeLevel);
-			startBgThread();
-		}
-	}
+			// Setup light sensor
+			mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+			mLightSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+			startSensingLight();
 
-	@Override
-	public int onStartCommand(Intent intent, int flags, int startId) {
-		String action = intent.getAction();
-
-		if (action != null && action.equals("increase")) {
-			increaseLevel();
-		} else if (action != null && action.equals("decrease")) {
-			decreaseLevel();
-		} else {
-			Log.d(mTag, "starting service");
-			init();
+			startNotification();
 		}
 		return super.onStartCommand(intent, flags, startId);
 	}
 
 	@Override
 	public IBinder onBind(Intent intent) {
-		Log.d(mTag, "binding service");
-		init();
-		return new MainServiceBinder();
+		return null;
 	}
 
-	private void startBgThread() {
-		mBgThread.start();
-		startNotification();
+	@Override
+	public void onDestroy() {
+		Log.d(mTag, "stopping service");
+		mPrefs.unregisterOnSharedPreferenceChangeListener(this);
+		unregisterReceiver(mScreenReceiver);
+		this.stopForeground(true);
+		super.onDestroy();
 	}
 
-	private void stopBgThread() {
-		mBgThread.finish();
-		mBgThread.interrupt();
-		stopForeground(true);
+	private void increaseBrightness() {
+		setRelativeLevel(mRelativeLevel + 10, true);
+	}
+
+	private void decreaseBrightness() {
+		setRelativeLevel(mRelativeLevel - 10, true);
+	}
+
+	private void startSensingLight() {
+		if (mIsScreenOn) {
+			mSensorManager.registerListener(this, mLightSensor,
+					SensorManager.SENSOR_DELAY_NORMAL);
+		}
+	}
+
+	private void startSensingLight(long delay) {
+		new Handler(Looper.myLooper()).postDelayed(new Runnable() {
+
+			@Override
+			public void run() {
+				startSensingLight();
+			}
+
+		}, delay);
+	}
+
+	private void stopSensingLight() {
+		mSensorManager.unregisterListener(this, mLightSensor);
+	}
+
+	private void setRelativeLevel(int relativeLevel) {
+		setRelativeLevel(relativeLevel, false);
+	}
+
+	private void setRelativeLevel(int relativeLevel, boolean saveToPrefs) {
+		relativeLevel = Math.min(Math.max(relativeLevel, 0), 255);
+		Log.d(mTag, "new relative level: " + relativeLevel);
+		if (mRelativeLevel != relativeLevel) {
+			mRelativeLevel = relativeLevel;
+			updateBrightness();
+
+			if (saveToPrefs) {
+				mPrefs.edit().putInt("relativeLevel", mRelativeLevel).commit();
+			}
+		}
+	}
+
+	private void updateBrightness() {
+		int brightness = (int) ((mLux / 30) + (mRelativeLevel / 2));
+		Settings.System.putInt(getContentResolver(),
+				Settings.System.SCREEN_BRIGHTNESS, brightness);
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		if (event.sensor.getType() == Sensor.TYPE_LIGHT) {
+			// Ignore if updating too often
+			long currentTime = System.currentTimeMillis();
+			if ((currentTime - mLastSenseMs) < mSenseIntervalMs) {
+				return;
+			}
+
+			// Save last update time
+			mLastSenseMs = currentTime;
+
+			// Get lux level
+			float previousLux = mLux;
+			float newLux = event.values[0];
+
+			// Turn off light sensor and schedule next reading
+			stopSensingLight();
+			startSensingLight(mSenseIntervalMs);
+
+			// Only update if lux has changed
+			if (Math.abs(newLux - previousLux) > LUX_DIFF_THRESHOLD) {
+				Log.d(mTag, "new lux: " + newLux);
+				mLux = newLux;
+				updateBrightness();
+			} else {
+				Log.d(mTag, "not updating");
+			}
+		}
 	}
 
 	private void startNotification() {
@@ -90,37 +187,16 @@ public class MainService extends Service {
 		startForeground(1, notification.build());
 	}
 
-	void increaseLevel() {
-		mBgThread.getHandler().sendEmptyMessage(
-				BackgroundThread.ACTION_INCREASE_LEVEL);
-	}
-
-	void decreaseLevel() {
-		mBgThread.getHandler().sendEmptyMessage(
-				BackgroundThread.ACTION_DECREASE_LEVEL);
-	}
-
-	void setRelativeLevel(int relativeLevel) {
-		mBgThread.getHandler().sendEmptyMessage(relativeLevel);
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+		return; // No need for this
 	}
 
 	@Override
-	public boolean onUnbind(Intent intent) {
-		Log.d(mTag, "unbinding service");
-		return super.onUnbind(intent);
-	}
-
-	@Override
-	public void onDestroy() {
-		Log.d(mTag, "killing service");
-		unregisterReceiver(mScreenReceiver);
-		stopBgThread();
-		super.onDestroy();
-	}
-
-	class MainServiceBinder extends Binder {
-		void setRelativeLevel(int relativeLevel) {
-			MainService.this.setRelativeLevel(relativeLevel);
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+			String key) {
+		if ("relativeLevel".equals(key)) {
+			setRelativeLevel(sharedPreferences.getInt(key, 50));
 		}
 	}
 
@@ -128,14 +204,13 @@ public class MainService extends Service {
 		@Override
 		public void onReceive(Context context, Intent intent) {
 			if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
-				Log.d(mTag, "screen off");
-				mBgThread.getHandler().sendEmptyMessage(
-						BackgroundThread.ACTION_SCREEN_OFF);
+				mIsScreenOn = false;
+				stopSensingLight();
 			} else if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)) {
-				Log.d(mTag, "screen on");
-				mBgThread.getHandler().sendEmptyMessage(
-						BackgroundThread.ACTION_SCREEN_ON);
+				mIsScreenOn = true;
+				startSensingLight();
 			}
 		}
 	}
+
 }
